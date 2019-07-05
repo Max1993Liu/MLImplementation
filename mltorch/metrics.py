Fixed binary crossentropy calculation

mltorch/metrics.py
@@ -10,5 +10,5 @@ def mean_absolute_error(y_true, y_pred):
 
 def binary_crossentropy(y_true, y_pred, epsilon=1e-12):
     pred_clipped = torch.clamp(y_pred, epsilon, 1 - epsilon)
-    cse = -(y_true * torch.log(pred_clipped) + (1 - y_true) * torch.log(pred_clipped)).mean()
+    cse = -(y_true * torch.log(pred_clipped) + (1 - y_true) * torch.log(1 - pred_clipped)).mean()
     return cse